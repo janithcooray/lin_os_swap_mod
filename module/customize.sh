41e6b774d813f2a8fb8dbc663b531d7f1117e6ab
janithcooray <jvcooray@gmail.com>
2023-07-25 04:00:30 +0530

    Added an option during installtion to select Ram Priority

--- module/customize.sh
+++ module/customize.sh
@@ -19,7 +19,7 @@ set_perm_recursive $MODPATH 0 0 0755 0644
 . $MODPATH/util.sh || abort
 . $MODPATH/vars.sh || abort
 
-
+# Option - Select 8GB or 4GB
 function ask_install(){
     ui_print "  Select the Swap File size"
     ui_print "   Vol Up += 8GB"
@@ -33,8 +33,24 @@ function ask_install(){
     fi
 }
 
+# Option - Select Swap Priority
+function ask_zram_prior(){
+    ui_print "  Set Swap Priority above Zram?"
+    ui_print "   Vol Up += Yes"
+    ui_print "   Vol Down += No "
+    if $VKSEL; then
+        ui_print "  Setting to 0"
+        OVER_ZRAM_PRIOR=1
+    else
+        ui_print "  Setting to auto"
+        OVER_ZRAM_PRIOR=0
+    fi
+}
+
+# Create Swapfile
 function create_swapfile(){
     ui_print "- Trying to stop Existing Swapfile"
+    ui_print "  (This can take a few minutes, do not panic if it looks stuck)"
     swapoff /data/swap/swapfile
     rm -rf /data/swap
     mkdir /data/swap
@@ -46,14 +62,18 @@ function create_swapfile(){
     ui_print "- Making Swapfile!!!"
 }
 
+# Enable Swapfile settings
 function enable_swapfile(){
     ui_print "- Setting Swappiness to 75"
     sysctl vm.swappiness=75
+    echo $OVER_ZRAM_PRIOR > /data/swap/OVER_ZRAM_PRIOR
     ui_print "- Now Reboot and see if it works!!"
 }
 
-
+# Start install
 function custom_install() {
+    ui_print "- Please keep the screen on during installtion"
+    ui_print "- Version 1.2"
     ui_print "- Testing Volume keys... (10 Second timeout)"
     if keytest; then
         ui_print "- Using chooseport method for Volume keys"
@@ -65,8 +85,10 @@ function custom_install() {
         ui_print "  Unzip this module, and edit the file vars.sh"
         ui_print "  and change the value of 'SWAP_BIN_SIZE'='xxxx'(MB)"
         ui_print "  zip the file and flash again"
+        ui_print "  Using Default Swap Priority - auto"
     fi
     create_swapfile
+    ask_zram_prior
     enable_swapfile
 }
 
--- module/service.sh
+++ module/service.sh
@@ -3,12 +3,20 @@
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-#!/system/sbin/sh
-# vm_swap_ratio=100 is default, no need to set
-#sysctl vm.swap_ratio_enable=1
 rm /data/swap/swapfile.log
+
 sysctl vm.swappiness=75
+
 echo "-------------------" >> /data/swap/swapfile.log
+
 now=$(date)
+
 echo "$now" >> /data/swap/swapfile.log
-/system/bin/swapon /data/swap/swapfile >> /data/swap/swapfile.log
+
+OVER_ZRAM_PRIOR="$(cat /data/swap/OVER_ZRAM_PRIOR)"
+
+if [[ "$OVER_ZRAM_PRIOR" -eq 1 ]]; then
+    /system/bin/swapon -p 0 /data/swap/swapfile >> /data/swap/swapfile.log
+else
+    /system/bin/swapon /data/swap/swapfile >> /data/swap/swapfile.log
+fi
