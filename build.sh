58d8001c768f8df6c9b0c1ab52bf5b1e1d904012
janithcooray <jvcooray@gmail.com>
2024-01-27 13:36:00 +0530

    updated build script

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-rm module.zip
+#! /bin/zsh
+rm -f module.zip
+rm -f lin_os_swap_mod.zip
+rm -f swapfile_mod.zip
 7z a module.zip module/.
+cp module.zip lin_os_swap_mod.zip
+cp module.zip swapfile_mod.zip
