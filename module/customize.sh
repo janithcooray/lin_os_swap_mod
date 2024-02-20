janithcooray/lin_os_swap_mod

commit 84440c9f969341a65478003c07911a714c8169df

Fixed Value Mistakes

module/customize.sh
@@ -40,10 +40,10 @@ function ask_zram_prior(){
     ui_print "   Vol Down += No "
     if $VKSEL; then
         ui_print "  Setting to 0"
-        OVER_ZRAM_PRIOR=1
+        OVER_ZRAM_PRIOR=0
     else
         ui_print "  Setting to auto"
-        OVER_ZRAM_PRIOR=0
+        OVER_ZRAM_PRIOR=1
     fi
 }
 
@@ -65,7 +65,7 @@ function create_swapfile(){
 # Enable Swapfile settings
 function enable_swapfile(){
     ui_print "- Setting Swappiness to 99"
-    sysctl vm.swappiness=75
+    sysctl vm.swappiness=99
     echo $OVER_ZRAM_PRIOR > /data/swap/OVER_ZRAM_PRIOR
     ui_print "- Now Reboot and see if it works!!"
 }

commit 66f85b50fe96aed5035e1f7d4b80c6489288677d

Fix reversed logic for "Set Swap Priority above Zram?" option
This was caused by commit 84440c9 and is related to issue #10. OVER_ZRAM_PRIOR's values in customize.sh were reversed from what they should be, so answering 'Yes' would not set the swapfile's priority to 0 as expected.

module/customize.sh
@@ -40,10 +40,10 @@ function ask_zram_prior(){
     ui_print "   Vol Down += No "
     if $VKSEL; then
         ui_print "  Setting to 0"
-        OVER_ZRAM_PRIOR=0
+        OVER_ZRAM_PRIOR=1
     else
         ui_print "  Setting to auto"
-        OVER_ZRAM_PRIOR=1
+        OVER_ZRAM_PRIOR=0
     fi
 }
 