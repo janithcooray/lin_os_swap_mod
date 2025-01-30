ebedbb12a7ae00fd6abd96f7f1a0dcd12b446d17
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2025-01-31 06:07:43 +0800

    Used $SWAP_FILE_PATH instead of /data/swap

--- module/service.sh
+++ module/service.sh
@@ -3,60 +3,27 @@
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-echo "----------BOOT---------" >> /data/swap/swapfile.log
-function print_log(){
-    now=$(date)
-    echo "$now : $1 $2" >> /data/swap/swapfile.log
-}
-
-function check_file(){
-        print_log "Checking if $1 Exists"
-    if [ -e $1 ]; then
-        print_log "File exists."
-    else
-        print_log "File does not exist."
-    fi
-}
-
-function check_bin(){
-    if command -v $1 >/dev/null 2>&1; then
-        print_log "$1 exists."
-    else
-        print_log "$1 does not exist."
-    fi
-}
+echo "-------------------" >> $SWAP_FILE_PATH/swapfile.log
+now=$(date)
+echo "$now" >> $SWAP_FILE_PATH/swapfile.log
 
 # START BOOT SAFETY
 # We create a file before swap on and delete it after successful start
 # If the file exists on service boot, that means there has been an issue from the 
-# Module. Ask the user to share the /data/swap/swapfile.log file with devs
-check_file "/system/bin/swapon"
-check_bin "swapon"
-check_bin "sysctl"
-check_bin "whereis"
+# Module. Ask the user to share the $SWAP_FILE_PATH/swapfile.log file with devs
 
-if [ -e "/data/swap/INCOMPLETE" ]; then
-    print_log "Swap is disabled sue to fail safe mode. A previous session falied to start. Remove /data/swap/INCOMPLETE and reboot to disable."
+if [ -e "$SWAP_FILE_PATH/INCOMPLETE" ]; then
+    echo "$now : INCOMPLETE FILE still exists! Did it Fail to boot?" >> $SWAP_FILE_PATH/swapfile.log
 else
-    echo "INCOMPLETE" >> /data/swap/INCOMPLETE
-    print_log "Preparing to start.."
-    SWAP_FILE_PRIOR="$(cat /data/swap/SWAP_FILE_PRIOR)"
-    SWAPPINESS="$(cat /data/swap/SWAPPINESS)"
-    print_log "setting swappiness.."
-    sysctl vm.swappiness=$SWAPPINESS
-    # At this point if it fails, it will exit the script leaving /data/swap/INCOMPLETE
-    print_log "[OK] setting swappiness.."
+    echo "INCOMPLETE" >> $SWAP_FILE_PATH/INCOMPLETE
+    SWAP_FILE_PRIOR="$(cat $SWAP_FILE_PATH/SWAP_FILE_PRIOR)"
+    SWAPPINESS="$(cat $SWAP_FILE_PATH/SWAPPINESS)"
+    sysctl vm.swappiness=99
     if [[ "$SWAP_FILE_PRIOR" -eq 0 ]]; then
-        print_log "Starting swap with auto priority"
-        swapon /data/swap/swapfile >> /data/swap/swapfile.log
-        print_log "No Errors Reported"
+        /system/bin/swapon $SWAP_FILE_PATH/swapfile >> $SWAP_FILE_PATH/swapfile.log
     else
-        print_log "Starting swap with $SWAP_FILE_PRIOR priority"
-        swapon -p $SWAP_FILE_PRIOR /data/swap/swapfile >> /data/swap/swapfile.log
-        print_log "No Errors Reported"
+        /system/bin/swapon -p $SWAP_FILE_PRIOR $SWAP_FILE_PATH/swapfile >> $SWAP_FILE_PATH/swapfile.log
     fi
 fi
-FREE_M="$(free -h)"
-print_log "Free Mem - " $FREE_M
 # Service BOOT OK!
-rm /data/swap/INCOMPLETE
+rm $SWAP_FILE_PATH/INCOMPLETE
